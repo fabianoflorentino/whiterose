0aaed4a26431945d021d36eabc979bfb6c2c00a5
Fabiano Santos Florentino <fabianoratm@gmail.com>
2025-08-24 11:54:02 -0300

    feat: enhance LoadDotEnv function to display environment variable configuration on load failure

--- utils/dot_env.go
+++ utils/dot_env.go
@@ -7,6 +7,18 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const envVars string = `
+Configure a .env file in your home directory with the following variables:
+
+If you not use a custom settings, create the file with the variables empty,
+the program using the default values.
+
+GIT_USERNAME=
+GIT_TOKEN=
+SSH_KEY_PATH=
+SSH_KEY_NAME=
+`
+
 func LoadDotEnv() error {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
@@ -17,7 +29,8 @@ func LoadDotEnv() error {
 
 	if err := godotenv.Load(dotEnvFile); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to load .env file: %v\n", err)
-		os.Exit(1)
+		fmt.Printf("%s\n", envVars)
+		os.Exit(125)
 	}
 
 	return nil
