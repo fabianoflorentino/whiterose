a0aed1a40005a5402f40a29890ad6de397a4db4f
Fabiano Santos Florentino <fabianoratm@gmail.com>
2025-08-23 23:27:57 -0300

    feat: load environment variables on initialization

--- main.go
+++ main.go
@@ -3,8 +3,15 @@ Copyright © 2025 FABIANO SANTOS FLORENTINO <FABIANORATM@GMAIL.COM>
 */
 package main
 
-import "github.com/fabianoflorentino/whiterose/cmd"
+import (
+	"github.com/fabianoflorentino/whiterose/cmd"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	cmd.Execute()
+}
+
+func init() {
+	godotenv.Load()
 }
