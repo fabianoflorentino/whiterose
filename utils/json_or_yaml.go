6cefb65a2749ffa5a38c6b19ea4ccbf79cdd9eda
Fabiano Santos Florentino <fabianoratm@gmail.com>
2025-11-09 10:15:21 -0300

    refactor: streamline YmlOrYamlExistsInHomeDir function and improve file extension checks

--- utils/json_or_yaml.go
+++ utils/json_or_yaml.go
@@ -1,6 +1,13 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
+
+var (
+	extensionsYAML = []string{".yml", ".yaml"}
+)
 
 func YmlOrYamlExistsInHomeDir() bool {
 	homeDir, err := os.UserHomeDir()
@@ -8,24 +15,20 @@ func YmlOrYamlExistsInHomeDir() bool {
 		return false
 	}
 
-	ymlPath := homeDir + "/" + ".config.yml"
-	yamlPath := homeDir + "/" + ".config.yaml"
-
-	if _, err := os.Stat(ymlPath); err == nil {
-		return true
-	}
-
-	if _, err := os.Stat(yamlPath); err == nil {
-		return true
+	for _, e := range extensionsYAML {
+		if _, err := os.Stat(homeDir + "/.config" + e); err == nil {
+			return true
+		}
 	}
 
 	return false
 }
 
 func IsFileJSON(file string) bool {
-	return len(file) >= 5 && file[len(file)-5:] == ".json"
+	return len(file) >= 5 && filepath.Ext(file) == ".json"
 }
 
 func IsFileYAML(file string) bool {
-	return (len(file) >= 5 && file[len(file)-5:] == ".yaml") || (len(file) >= 4 && file[len(file)-4:] == ".yml")
+	ext := filepath.Ext(file)
+	return ext == ".yaml" || ext == ".yml"
 }
