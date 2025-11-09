3ac7f0ca7b1b8d822dd661b9fa66206e026b59b2
Fabiano Santos Florentino <fabianoratm@gmail.com>
2025-11-09 09:47:24 -0300

    feat: implement YmlOrYamlExistsInHomeDir function to check for YAML config files in home directory

--- utils/json_or_yaml.go
+++ utils/json_or_yaml.go
@@ -1,5 +1,27 @@
 package utils
 
+import "os"
+
+func YmlOrYamlExistsInHomeDir() bool {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return false
+	}
+
+	ymlPath := homeDir + "/" + ".config.yml"
+	yamlPath := homeDir + "/" + ".config.yaml"
+
+	if _, err := os.Stat(ymlPath); err == nil {
+		return true
+	}
+
+	if _, err := os.Stat(yamlPath); err == nil {
+		return true
+	}
+
+	return false
+}
+
 func IsFileJSON(file string) bool {
 	return len(file) >= 5 && file[len(file)-5:] == ".json"
 }
