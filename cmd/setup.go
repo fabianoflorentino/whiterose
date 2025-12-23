c0421e9749ff23ea0dbbc7fc5f6834c5a4fe4d85
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-12-23 08:51:15 -0300

    fix: enhance setup command to handle flags for pre-requisites and repository cloning

--- cmd/setup.go
+++ cmd/setup.go
@@ -4,6 +4,8 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/fabianoflorentino/whiterose/setup"
 	"github.com/spf13/cobra"
 )
@@ -19,8 +21,19 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		setup.PreReq()
-		setup.GitCloneRepository()
+		switch {
+		case cmd.Flags().Changed("all"):
+			setup.PreReq()
+			setup.GitCloneRepository()
+		case cmd.Flags().Changed("pre-req"):
+			setup.PreReq()
+		case cmd.Flags().Changed("repos"):
+			setup.GitCloneRepository()
+		default:
+			if err := cmd.Help(); err != nil {
+				fmt.Println(err)
+			}
+		}
 	},
 }
 
@@ -32,6 +45,9 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// setupCmd.PersistentFlags().String("foo", "", "A help for foo")
+	setupCmd.PersistentFlags().BoolP("all", "a", false, "Check and install pre-requisites")
+	setupCmd.PersistentFlags().BoolP("pre-req", "p", false, "Check and install pre-requisites")
+	setupCmd.PersistentFlags().BoolP("repos", "r", false, "Clone Git repository")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
