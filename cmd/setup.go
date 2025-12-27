e39baed1a51d1221c40db01d0c7225e36f865f09
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-12-27 10:23:51 -0300

    fix: update setup command descriptions for clarity and usability

--- cmd/setup.go
+++ cmd/setup.go
@@ -13,13 +13,12 @@ import (
 // setupCmd represents the setup command
 var setupCmd = &cobra.Command{
 	Use:   "setup",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Configura o ambiente inicial do projeto",
+	Long: `The 'setup' command prepares the environment for using the whiterose project.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+It can be used to:
+- Check and install required prerequisites (such as system dependencies and mandatory tools);
+- Clone the necessary git repositories for the project to work.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		switch {
 		case cmd.Flags().Changed("all"):
